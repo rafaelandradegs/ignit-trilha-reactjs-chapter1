3ad180f3ce4b3495eddfb98bdca9a86fc6af3608
Rafael A Gonçalves <rafaelandradegs@gmail.com>
2021-08-31 09:05:37 -0300

    Estilização da listagem

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,9 +4,7 @@ export function RepositoryItem(props) {
       <strong>{props.repository.name ?? 'Default'}</strong>
       <p>{props.repository.description}</p>
 
-      <a href="https://github.com/unform/unform" target="_blank">
-        {props.repository.link}
-      </a>
+      <a href="">{props.repository.link}</a>
     </li>
   )
 }
